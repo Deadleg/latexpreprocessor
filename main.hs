0dfde0b1599441c8f9c11dce201f47d17abb9b6d
William Kerrigan <Deadleg@gmail.com>
2015-01-18 20:43:45 +1300

    First implementation for bold and bold emphasis.

--- main.hs
+++ main.hs
@@ -3,28 +3,60 @@ import Control.Monad
 import Text.ParserCombinators.Parsec
 import Data.List
 
-type latex = String
+type Latex = String
 
 emphasisSymbol :: Parser Char
 emphasisSymbol = char '*'
 
-italicizedChar = noneOf "*"
+boldSymbol :: Parser String
+boldSymbol = string "**"
 
-italics = do emphasisSymbol
-             content <- many1 italicizedChar
-             emphasisSymbol
-             return content
+beginBoldEmphasisSymbol :: Parser String
+beginBoldEmphasisSymbol = string "*_"
 
-bodyText = italics <|> many1 (noneOf "*")
+endBoldEmphasisSymbol :: Parser String
+endBoldEmphasisSymbol = string "_*"
+
+emphacizedChar :: Parser Char
+emphacizedChar = noneOf "*"
+
+boldEmphasizedChar :: Parser Char
+boldEmphasizedChar = try (do char '_'
+                             noneOf "*")
+                 <|> noneOf "_"
+                 <?> "Didn't find bold emph."
+
+boldChar :: Parser Char
+boldChar = try (do char '*'
+                   noneOf "*")
+       <|> noneOf "*"
+       <?> "Didn't find bold." 
+
+boldEmphasis = do beginBoldEmphasisSymbol
+                  content <- many1 boldEmphasizedChar
+                  endBoldEmphasisSymbol
+                  return content
+
+emphasis = do emphasisSymbol
+              content <- many1 emphacizedChar
+              emphasisSymbol
+              return content
+
+bold = do boldSymbol
+          content <- many1 boldChar
+          boldSymbol
+          return content
+
+bodyText = try (boldEmphasis) <|> try (bold) <|> try (emphasis) <|> many1 (noneOf "*")
 
 htexFile = many bodyText
 
 readInput :: String -> [Latex]
 readInput input = case parse htexFile "" input of
     Left err  -> ["No match " ++ show err]
-    Right val -> fmap ("Match found:\n"++) val
+    Right val -> "Match found:" : val
 
 main = do 
-    contents <- readFile "E:/Google Drive/Code/latexprecompiler/input.htex"
+    contents <- readFile "E:\\Google Drive\\Code\\latexpreprocessor\\input.htex"
     putStrLn $ intercalate "\n" (readInput contents)
 

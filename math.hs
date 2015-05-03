835d9991ab455fb93403777f7de6610ba2af4016
William Kerrigan <Deadleg@gmail.com>
2015-05-03 20:08:33 +1200

    Some refactorijg? Dun remember

--- math.hs
+++ math.hs
@@ -1,21 +1,14 @@
 {-# LANGUAGE TemplateHaskell #-}
 import MathProcessor
 import Data.Data
-import Data.Typeable
 import MathDefaults
 
-gamma = Expression { getKey="gamma", exprType=Var }
-lambda = Expression { getKey="lambda", exprType=Var }
-epsilon = Expression { getKey="epsilon", exprType=Var }
-x = Expression { getKey="x", exprType=Var }
-theta = Expression { getKey="theta", exprType=BinaryFunc gamma lambda constructInfix }
-
-leftEquation = Expression { getKey="fasda", exprType=MultiFunc [theta,gamma,x,epsilon] constructMulti }
-
---rightEquation = (l)
-
---equationOne :: (Expression, Expression)
---equationOne = (leftEquation, rightEquation)
+gamma        = Expression { getKey="gamma"    , exprType=Var }
+lambda       = Expression { getKey="lambda"   , exprType=Var }
+epsilon      = Expression { getKey="epsilon"  , exprType=Var }
+x            = Expression { getKey="x"        , exprType=Var }
+theta        = Expression { getKey="plus"    , exprType=BinaryFunc gamma lambda constructInfix }
+leftEquation = Expression { getKey="function1", exprType=MultiFunc [theta,gamma,x,epsilon] constructMulti }
 
 main = do
         latex <- displayEquation leftEquation "vars.txt"
